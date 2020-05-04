3d8952e1cbd278aa062c37a793a26b39b6b29a25
shivam thapar <shivam.thapar@quikr.com>
2020-05-05 01:55:01 +0530

    List view first draft

--- modules/constants.js
+++ modules/constants.js
@@ -2,7 +2,12 @@ const constants = {
     fileName: 'hosts',
     backupFileName: 'hosts.bkp.manage-hosts',
     filePath: '/etc/',
-    defaultDateFormat: 'YYYY-MM-DDTHH-mm-ss'
+    defaultDateFormat: 'YYYY-MM-DDTHH-mm-ss',
+    regex: {
+        ipDomain: /^(#+)?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})([\s\S]+)$/gi,
+        comment: /^(#+)?([\s\S]+)$/gi
+    },
+    env: ['dev','local','prod','uat','stage']
 }
 
 module.exports = constants;
--- modules/processes.js
+++ modules/processes.js
@@ -1,5 +1,7 @@
 const utils = require("./utils");
-const fs = require('fs');
+const contants = require("./constants");
+const fs = require("fs");
+const os = require("os");
 /**
  * This file exports all available processing that can be done on the hosts file
  */
@@ -12,15 +14,128 @@ function processes() {
 
     // Function that lists the ip and domains in hosts file
     self.list = function() {
-        console.log("Populating list for " + utility.getFullFilePath());
+        const fileData = readFile(utility.getFullFilePath());
+        let lineInfo = [];
+        let prevLineInfo = newLineInfo();
+        fileData.forEach((line, lineNumber) => {
+            let currentLineInfo = getLineInfo(line, lineNumber);
+            if (!prevLineInfo.isValid && !prevLineInfo.isEmpty && currentLineInfo.isValid) {
+                lineInfo[lineNumber - 1] = markAsGroup(prevLineInfo);
+            }
+            lineInfo[lineNumber] = currentLineInfo;
+            prevLineInfo = currentLineInfo;
+        });
+        print(lineInfo);
     }
 
     // Function that backs up the current hosts file to the file provided by user.
     self.backup = function() {
-        var backupFilePath = utility.getBackupFilePath();
-        fs.copyFileSync(utility.getFullFilePath(), backupFilePath);
-        console.log("Backed up successfully to " + backupFilePath);
+        fs.copyFileSync(utility.getFullFilePath(), utility.getBackupFilePath());
+        console.log("Backed up successfully to " + utility.getBackupFilePath());
     }
+
+    function print(lineInfo) {
+        lineInfo.forEach((info) => {
+            let output = '';
+            if (!info.isEmpty) {
+                if (info.isValid) {
+                    output += info.lineNumber;
+                    output += ' ' + info.ip;
+                    info.domains.forEach((domain) => {
+                        output += ' ' + domain;
+                    });
+                    output += info.isActive ? ' ACTIVE' : ' DISABLED'
+                } else if(info.groupInfo.name) {
+                    output += info.lineNumber;
+                    output += ' Group - ' + info.groupInfo.name;
+                    if (info.groupInfo.env !== '') {
+                        output += ' | Environment - ' + info.groupInfo.env;
+                    }
+                }
+            }
+            if (output !== '') {
+                console.log(output);
+            }
+        });
+    }
+
+    function readFile(filePath) {
+        const file = fs.readFileSync(filePath).toString();
+        return file.split(os.EOL);
+    }
+
+    function getLineInfo(lineData, lineNumber) {
+        var info = newLineInfo(lineNumber);
+        info.rawData = lineData;
+        lineData = lineData.trim();
+        info.isEmpty = lineData === '';
+        if (!info.isEmpty) {
+            let ipDomainInfo = [...lineData.matchAll(contants.regex.ipDomain)];
+            if (ipDomainInfo.length) {
+                info.lineData = lineData;
+                info.isValid = true;
+                info.ip = ipDomainInfo[0][2].trim();
+                info.domains = ipDomainInfo[0][3].trim().split(/\s/);
+                info.isActive = ipDomainInfo[0][1] === undefined
+            } else {
+                let commentInfo = [...lineData.matchAll(contants.regex.comment)];
+                let comment = commentInfo[0][2].trim().replace(/#+/g, '');
+                if (comment !== '') {
+                    info.lineData = commentInfo[0][2].trim();
+                } else {
+                    info.isEmpty = true;
+                }
+            }
+        }
+        return info;
+    }
+
+    function markAsGroup(lineInfo) {
+        var env = '';
+        contants.env.forEach((e) => {
+            if (lineInfo.lineData.toLowerCase().indexOf(e) !== -1) {
+                env = e;
+                return;
+            }
+        });
+        lineInfo.groupInfo = {
+            name: lineInfo.lineData,
+            env: env
+        };
+        lineInfo.isGroupInfo = true;
+        return lineInfo;
+    }
+
+    return self;
+}
+
+function newLineInfo(lineNumber) {
+    var self = {
+        lineNumber: lineNumber + 1,
+
+        rawData: '',
+
+        isEmpty: false,
+
+        // Is valid ip domain enrty
+        isValid: false,
+
+        isGroupInfo: false,
+
+        lineData: '',
+
+        // Details of group info
+        groupInfo: {},
+
+        // IP
+        ip: '',
+
+        // Domains
+        domains: [],
+
+        // Is entry active
+        isActive: false
+    };
 
     return self;
 }
